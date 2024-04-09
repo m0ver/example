93a60f7c658b002943011b8c20a562e0da3a74b5
James ZHOU <moverinfo@gmail.com>
2024-04-10 00:09:18 +0800

    first commit

---
+++ src/main/java/tinystruct/examples/example.java
@@ -0,0 +1,38 @@
+package tinystruct.examples;
+
+
+import org.tinystruct.AbstractApplication;
+import org.tinystruct.ApplicationException;
+import org.tinystruct.system.annotation.Action;
+
+public class example extends AbstractApplication {
+
+    @Override
+    public void init() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public String version() {
+        return "1.0";
+    }
+
+    @Action("praise")
+    public String praise() {
+        return "Praise to the Lord!";
+    }
+
+    @Action("say")
+    public String say() throws ApplicationException {
+        if (null != this.context.getAttribute("--words"))
+            return this.context.getAttribute("--words").toString();
+
+        throw new ApplicationException("Could not find the parameter <i>--words</i>.");
+    }
+
+    @Action("say")
+    public String say(String words) {
+        return words;
+    }
+
+}
